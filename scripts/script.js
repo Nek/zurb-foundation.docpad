4679b50844bfdc429738aab708d8107aad5dba36
Geert-Jan Brits <info@clearskyabove.com>
2013-08-09 19:05:06 +0200

    d106650 complete 0.1

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+/**
+ * Your code goes here. Optionally, you may want to include it in the grunt minify task.
+ * Note that (following Foundation) $ may be either Zepto (modern browsers) or Jquery ( < IE9)
+ * If you don't want that, change the grunt minify task.
+ */
+$(function(){
+	
+});
+
